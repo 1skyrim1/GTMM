74e7bca8c2cbaf341d898db16116c06cc0feec55
Philip Andel <philip.andel@web.de>
2025-05-13 23:41:36 +0200

    fixed coal recipe type conflict

--- src/main/java/com/gtmoremultis/gtmm/data/GTMMRecipeTypes.java
+++ src/main/java/com/gtmoremultis/gtmm/data/GTMMRecipeTypes.java
@@ -7,13 +7,14 @@ import com.gregtechceu.gtceu.api.recipe.GTRecipeType;
 import com.gregtechceu.gtceu.common.data.GTRecipeTypes;
 import com.gregtechceu.gtceu.common.data.GTSoundEntries;
 import com.lowdragmc.lowdraglib.gui.texture.ProgressTexture;
+import com.lowdragmc.lowdraglib.gui.texture.ResourceTexture;
 
 public class GTMMRecipeTypes {
-    public static final GTRecipeType CoAL_RECIPES = GTRecipeTypes.register("component_assembly_line", GTRecipeTypes.MULTIBLOCK)
+    public static final GTRecipeType CoAL_RECIPES = GTRecipeTypes.register("component_assline", GTRecipeTypes.MULTIBLOCK)
             .setMaxIOSize(12, 1, 8, 0)
             .setEUIO(IO.IN)
             .prepareBuilder(gtRecipeBuilder -> gtRecipeBuilder.EUt(GTValues.VA[GTValues.LV]))
-            .setSlotOverlay(false, false, GuiTextures.CIRCUIT_OVERLAY)
+            .setSlotOverlay(false, false, new ResourceTexture("gtmm:textures/gui/progressbar/component_assline.png"))
             .setProgressBar(GuiTextures.PROGRESS_BAR_ASSEMBLY_LINE, ProgressTexture.FillDirection.LEFT_TO_RIGHT)
             .setMaxTooltips(4)
             .setSound(GTSoundEntries.ASSEMBLER);
